8755201fee45bdfc7e61883eb2f267b29ba6c3d5
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-04 18:11:18 +0100

    Update Header component to include search functionality

--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -1,10 +1,24 @@
-import { Form, Link } from "@remix-run/react";
-import { authenticator } from "~/services/auth.server";
+import { Form, Link, useActionData } from "@remix-run/react";
+
 
 export default function Header({user}) {
+    const event = useActionData();
   return (
-    <div className="grid grid-cols-2 p-8 align-middle text-slate-50 bg-slate-800">
+    <div className="grid grid-cols-3 p-8 align-middle text-slate-50 bg-slate-800">
         <Link to="/" className="decoration-transparent"><h1 className="text-3xl font-bold">Event Planer</h1></Link>
+       <Form method="POST">
+            <input className="p-2 text-slate-700" type="search" name="search" placeholder="Search" />
+            <button name="_action" value="search" type="submit">Search</button>
+            {
+                !event ? null : event.map((entry) => {
+                    return <>
+                        <Link className="search-item" to={`/event/${entry._id}`} key={entry._id}>
+                            <p>{entry.title}</p>
+                        </Link>
+                    </>
+                })
+            }
+       </Form>
         <section className="text-right" >
             {
                 user?.user ? <>
