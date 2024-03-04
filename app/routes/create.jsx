43d6a3bb489bbc4bc695ea0daf65098675e441f6
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-04 13:10:02 +0100

    Refactor Create component and add useFetcher hook

--- app/routes/create.jsx
+++ app/routes/create.jsx
@@ -1,6 +1,7 @@
 import { Form } from "@remix-run/react";
 import { authenticator } from "~/services/auth.server";
 import mongoose from "mongoose";
+import { useFetcher } from "react-router-dom";
 
 export const loader = async ({ request }) => {
     await authenticator.isAuthenticated(request, {
@@ -10,29 +11,35 @@ export const loader = async ({ request }) => {
 };
 
 export default function Create(){
+    const fetcher = useFetcher();
     return (
         <div className="p-8 text-slate-50 bg-slate-900">
             <h1 className="text-3xl font-bold">Create Event</h1>
-            <Form method="post">
-                <label htmlFor="title">Title</label>
-                <input className="block p-2 text-slate-500" type="text" id="title" name="title" />
-                <label htmlFor="description">Description</label>
-                <textarea className="block p-2 text-slate-500" id="description" name="description" />
-                <label htmlFor="date">Date</label>
-                <input className="block p-2 text-slate-500" type="date" id="date" name="date" />
-                <button className="bg-slate-300 p-3 px-11 mt-3" type="submit">Create Event</button>
-            </Form>
+            <fetcher.Form method="post">
+                <fieldset disabled={(fetcher.state === "submitting") ? true : false}>
+                    <label htmlFor="title">Title</label>
+                    <input className="block p-2 text-slate-500" type="text" id="title" name="title" />
+                    <label htmlFor="description">Description</label>
+                    <textarea className="block p-2 text-slate-500" id="description" name="description" />
+                    <label htmlFor="date">Date</label>
+                    <input className="block p-2 text-slate-500" type="date" id="date" name="date" />
+                </fieldset>
+                <button  className="bg-slate-300 p-3 px-11 mt-3" type="submit">Create Event</button>
+            </fetcher.Form>
         </div>
     );
 }
 
 export const action = async ({ request }) => {
-    await authenticator.isAuthenticated(request, {
-        failureRedirect: "/login"
+    const user = await authenticator.isAuthenticated(request, {
+        failureRedirect: "/login",
     });
 
+    
     const formData = await request.formData();
     const data = Object.fromEntries(formData);
+    const userId = new mongoose.Types.ObjectId(user._id);
+    data.useriD = userId;
 
     const NewEvent = await mongoose.models.Entry.create(data);
 
