9c555a28e86dcf3678b96cc6bc5f5ef9c00f4117
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-04 09:43:18 +0100

    Add login and signup forms

--- app/routes/login.jsx
+++ app/routes/login.jsx
@@ -1,3 +1,5 @@
+import { Form } from "@remix-run/react";
+import "../Styles/login-signup.css";
 export function loader() {
   return { message: "Hello from the loader!" };
 }
@@ -5,11 +7,27 @@ export function loader() {
 export default function Login() {
   return (
     <div className="p-8 text-slate-50 bg-slate-900">
-      <h1 className="text-3xl font-bold">Login</h1>
+        <h1 className="text-3xl font-bold">Login</h1>
+        <Form method="post">
+            <div>
+                <label htmlFor="email">Email</label>
+                <input className="" type="email" id="email" name="email" />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" />
+            </div>
+            <div>
+                <button type="submit">Signup</button>
+            </div>
+        </Form>
     </div>
   );
 }
 
-export function action(){
+export function action({request}){
+    const formData = request.formData();
+    const data = Object.fromEntries(formData);
 
+    console.log(data);
 }
