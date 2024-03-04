fbaebf495b2f4075a66819f1ebec1e5f6e32bce7
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-04 13:09:41 +0100

    Update Card component to display title, description, and date

--- app/components/Card/index.jsx
+++ app/components/Card/index.jsx
@@ -3,7 +3,9 @@ export default function Card({entry}) {
     
     return (
         <div className="card p-8 text-slate-50 bg-slate-900">
-            <h1 className="text-1xl font-bold">{entry.text}</h1>
+            <h1 className="text-1xl font-bold">{entry.title}</h1>
+            <p>{entry.description}</p>
+            <time>{entry.date}</time>
         </div>
     );
 }
