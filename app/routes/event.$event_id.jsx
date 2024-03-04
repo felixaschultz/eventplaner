e2411942338f8d61071f6eca368885428055a429
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-04 15:21:36 +0100

    Refactor event route component to display event details and attendances

--- app/routes/event.$event_id.jsx
+++ app/routes/event.$event_id.jsx
@@ -6,7 +6,6 @@ export async function loader({params, request}){
     const eventId = new mongoose.Types.ObjectId(params.event_id);
 
     const event = await mongoose.models.Entry.findOne({_id: eventId});
-    console.log(event);
     if(!event || event.public === false){
         throw new Response(null, {
             status: 404,
@@ -19,9 +18,22 @@ export async function loader({params, request}){
 
 export default function Event(){
     const {event} = useLoaderData();
+    
     return (
-        <div className="p-8 text-slate-50 bg-slate-900">
-            <h1 className="text-3xl font-bold">{event?.title}</h1>
+        <div className="grid grid-cols-2 gap-9 p-8 text-slate-50 bg-slate-900">
+            <section>
+                <h1 className="text-3xl font-bold">{event?.title}</h1>
+                <p>{event?.description}</p>
+            </section>
+            <section>
+                <h2 className="text-3xl font-bold">{new Date(event?.date).toLocaleString("da-DK")}</h2>
+                <h3>Attendancies</h3>
+                {
+                    event?.attendances?.map((attendance) => (
+                        <p key={attendance._id}>{attendance.useriD}</p>
+                    ))
+                }
+            </section>
         </div>
     );
 }
