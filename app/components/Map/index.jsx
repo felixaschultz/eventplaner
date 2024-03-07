fa58a41643a78253805507acd7a081549d58b7dc
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-07 10:27:36 +0100

    Refactor Map component to remove iframe and add map container div

--- app/components/Map/index.jsx
+++ app/components/Map/index.jsx
@@ -1,15 +1,13 @@
 
+export const loader = async () => {
+    return null;
+};
+
 export default function Map({ place }){
+
     return (
-        <iframe 
-            width="100%" 
-            height="400" 
-            frameBorder="0" 
-            scrolling="no" 
-            marginHeight="0" 
-            marginWidth="0" 
-            title={place} 
-            src={`https://maps.google.com/maps?q=${place}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
-        />
+        <div className="map-container">
+
+        </div>
     )
 }
