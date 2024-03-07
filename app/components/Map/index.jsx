459b07ea82f0905f44686e57b136d026403f7a64
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-07 18:50:17 +0100

    Add responsive map with Google Maps embed

--- app/components/Map/index.jsx
+++ app/components/Map/index.jsx
@@ -6,8 +6,17 @@ export const loader = async () => {
 export default function Map({ place }){
 
     return (
-        <div className="map-container">
-
+        <div className="map-container mt-2">
+            <iframe
+                title="map"
+                width="100%"
+                height="300"
+                frameBorder="0"
+                scrolling="no"
+                marginHeight="0"
+                marginWidth="0"
+                src={`https://maps.google.com/maps?q=${place}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
+            ></iframe>
         </div>
     )
 }
