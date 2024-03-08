230adab43feca147bb6ac9f4bf8ba5e5380c6608
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-08 13:31:37 +0100

    Update Messenger component to display profile pictures

--- app/routes/messenger._index.jsx
+++ app/routes/messenger._index.jsx
@@ -1,7 +1,7 @@
 import { authenticator } from "~/services/auth.server";
 import mongoose from "mongoose";
 import { redirect } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 
 export const loader = async ({ params, request }) => {
     const user = await authenticator.isAuthenticated(request);
@@ -25,28 +25,38 @@ export const loader = async ({ params, request }) => {
 export default function Messenger() {
     const {currentChat, user} = useLoaderData();
     return (
-        <div className="p-3">
-            <h1 className="text-2xl text-bold">Messenger</h1>
+        <div className="grid grid-cols-2 p-3 bg-slate-900 text-slate-200">
             <div>
+                <h1 className="text-2xl text-bold">Chats</h1>
                 {
                     (currentChat?.length > 0) ? (
                         currentChat?.map((chat) => (
-                            <div key={chat._id}>
-                                <Link className="block" to={`/messenger/${chat._id}`}>
-                                    <h2 className="text-xl">
-                                        {
-                                            chat?.participants?.map((participant) => (
-                                                participant
-                                            ))
-                                        }
-                                    </h2>
-                                </Link>
-                            </div>
+                            <Link key={chat._id} className="block p-2" to={`/messenger/${chat._id}`}>
+                                <h2 className="text-xl">
+                                    {
+                                        chat?.participants?.map((participant) => (
+                                            participant
+                                        ))
+                                    }
+                                </h2>
+                            </Link>
                         ))
                     ) : (currentChat) ? (
-                        <Link to={`/messenger/${currentChat._id}`} key={currentChat._id}>
+                        <Link className="block p-2" to={`/messenger/${currentChat._id}`} key={currentChat._id}>
                             <h2>
-                                {(currentChat?.messages[0].sender.name !== user.name) ? currentChat?.messages[0].sender.name : currentChat?.messages[0].receiver.name}
+                                {(currentChat?.messages[0].sender.name !== user.name) ? 
+                                    <>
+                                        <img className="profile-picture" src={(currentChat?.messages[0].sender.image) ? currentChat?.messages[0].sender.image : "https://scontent-uc-d2c-7.intastellar.com/a/s/ul/p/avtr46-img/profile_standard.jpg"} alt={
+                                            currentChat?.messages[0].sender.name
+                                        } />
+                                        {currentChat?.messages[0].sender.name}
+                                    </> :
+                                     <>
+                                        <img className="profile-picture" src={(currentChat?.messages[0].receiver.image) ? currentChat?.messages[0].receiver.image : "https://scontent-uc-d2c-7.intastellar.com/a/s/ul/p/avtr46-img/profile_standard.jpg"} alt={
+                                            currentChat?.messages[0].receiver.name
+                                        } />
+                                        {currentChat?.messages[0].receiver.name}
+                                    </>}
                             </h2>
                         </Link>
                     ) : (
