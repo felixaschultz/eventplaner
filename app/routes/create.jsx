f0d500026d476c7e6648a2439e9c8c9fc20fca49
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-12 16:15:05 +0100

    Add date conversion and adjustment to UTC string format

--- app/routes/create.jsx
+++ app/routes/create.jsx
@@ -109,6 +109,9 @@ export const action = async ({ request }) => {
     const userId = new mongoose.Types.ObjectId(user._id);
     data.useriD = userId;
     data.image = newImage;
+    data.date = new Date(data.date);
+    data.date.setHours(data.date.getHours() + 1);
+    data.date = data.date.toUTCString();
 
     const NewEvent = await mongoose.models.Entry.create(data);
 
